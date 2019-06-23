e492d8e2124bf9298af5591284c61182ec74fb88
Michael Shepanski <mikes@nasuni.com>
2019-06-23 15:37:18 -0400

    show link in inventory

--- linkmanager/inventory.py
+++ linkmanager/inventory.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import os
 from linkmanager import HOME
 from linkmanager import log, utils
 _ = utils.short_home
@@ -13,6 +14,7 @@ def get_options(parser):
 def run_command(opts):
     """ List the inventory of links in LINKROOT. """
     for ftype, filepath in utils.iter_linkroot(opts.linkroot):
+        ftype = 'link' if os.path.islink(filepath) else ftype
         fsize = utils.safe_fsize(filepath)
         filepath = filepath.replace(opts.linkroot, HOME)
         log.info(f' {ftype:5}  {fsize:6}  {_(filepath)}')
