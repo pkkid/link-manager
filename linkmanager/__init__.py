5fc4e6cd32ee42e78beec0e57c329fb5a3d303c9
Michael Shepanski <mikes@nasuni.com>
2019-07-08 16:22:33 -0400

    Add logging definition back

--- linkmanager/__init__.py
+++ linkmanager/__init__.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
-import os, socket
+import logging
+import os
+import socket
+import sys
 
 FILE, DIR, LINK = 'file', 'dir', 'link'
 DELETED = 'DELETED'
@@ -8,3 +11,8 @@ LINKROOT, LINKDIR = 'LINKROOT', 'LINKDIR'
 HOME = os.path.expanduser('~')
 HOSTNAME = socket.gethostname()
 CONFIG = os.path.join(HOME, '.config/linkmanager.json')
+
+log = logging.getLogger('linkmanager')
+streamhandler = logging.StreamHandler(sys.stdout)
+streamhandler.setFormatter(logging.Formatter('%(message)s'))
+log.addHandler(streamhandler)
